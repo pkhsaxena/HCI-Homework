ea572dea7255c882d5ebd4fe34d1db90a880108f
Prakhar Saxena <saxenaprakhar1998@gmail.com>
2023-02-11 13:15:17 -0500

    added default page

---
+++ mysite/example/views.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from django.http import HttpResponse
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Prakhar!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
